e0fd362b2e88bb53a8973a20c83862e82589173d
parzuko <jivansh17@gmail.com>
2020-09-16 15:55:35 +0530

    add get_token file for OAUTH

---
+++ get_token.py
@@ -0,0 +1,10 @@
+"""
+Created By Jivansh Sharma 
+September 2020
+@parzuko
+
+"""
+
+# replace with your token location
+with open("C:\\Users\\jivan\\Desktop\\token.txt","r+") as token_file:
+    token = token_file.read()
