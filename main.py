b07eeb2c3f591e1918fc132da8301b39748d95fb
parzuko <jivansh17@gmail.com>
2020-09-16 18:36:50 +0530

    change prefix to '.'

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 from get_token import token as TOKEN
 from discord.ext import commands
 
-elvis = commands.Bot(command_prefix = "!")
+elvis = commands.Bot(command_prefix = ".")
 
 for cog in os.listdir("./cogs"):
     if cog.endswith('.py'):
