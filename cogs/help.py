e7a17adfb06ae13e9f99687979c532642ff91107
parzuko <jivansh17@gmail.com>
2020-09-17 01:09:21 +0530

    add new help command cog

---
+++ cogs/help.py
@@ -0,0 +1,19 @@
+import discord
+from discord.ext import commands
+
+class Help(commands.Cog):
+    def __init__(self, elvis):
+        self.elvis = elvis
+    
+    @commands.command(name = "help")
+    async def _help(self, ctx):
+        embed = discord.Embed(
+            color = discord.Color.from_rgb(244,66,146)
+        )
+        embed.set_author(name = "Elvis Commands",icon_url="https://media.giphy.com/media/be4QJiw3XbkioIsYbR/giphy.gif")
+        embed.set_thumbnail(url="https://media.giphy.com/media/be4QJiw3XbkioIsYbR/giphy.gif")
+        embed.add_field(name = "Basic", value = "`.help basic`")
+        await ctx.send(embed=embed)
+
+def setup(elvis):
+    elvis.add_cog(Help(elvis))
