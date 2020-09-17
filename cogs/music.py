53850fa0cff0a01155076cae96dd1f03720ab678
parzuko <jivansh17@gmail.com>
2020-09-17 22:22:07 +0530

    finish the YTDL class

--- cogs/music.py
+++ cogs/music.py
@@ -57,3 +57,64 @@ class YTDLSource(discord.PCMVolumeTransformer):
         self.dislikes = data.get('dislike_count')
         self.stream_url = data.get('url')
 
+    def __str__(self):
+        return f"**{self.title}** by **{self.uploader}**"
+
+    @classmethod
+    async def create_source(cls, ctx: commands.Context, search: str, *, loop: asyncio.BaseEventLoop = None):
+        loop = loop or asyncio.get_event_loop()
+
+        partial = functools.partial(cls.ytdl.extract_info, search, download=False, process=False)
+        data = await loop.run_in_executor(None, partial)
+
+        if data is None:
+            raise YTDLError(f"Couldn\'t find anything that matches `{search}`")
+
+        if 'entries' not in data:
+            process_info = data
+        else:
+            process_info = None
+            for entry in data['entries']:
+                if entry:
+                    process_info = entry
+                    break
+
+            if process_info is None:
+                raise YTDLError(f"Couldn\'t find anything that matches `{search}`")
+
+        webpage_url = process_info['webpage_url']
+        partial = functools.partial(cls.ytdl.extract_info, webpage_url, download=False)
+        processed_info = await loop.run_in_executor(None, partial)
+
+        if processed_info is None:
+            raise YTDLError(f'Couldn\'t fetch `{webpage_url}`')
+
+        if 'entries' not in processed_info:
+            info = processed_info
+        else:
+            info = None
+            while info is None:
+                try:
+                    info = processed_info['entries'].pop(0)
+                except IndexError:
+                    raise YTDLError(f"Couldn\'t retrieve any matches for `{webpage_url}`")
+
+        return cls(ctx, discord.FFmpegPCMAudio(info['url'], **cls.FFMPEG_OPTIONS), data=info)
+
+    @staticmethod
+    def parse_duration(duration: int):
+        minutes, seconds = divmod(duration, 60)
+        hours, minutes = divmod(minutes, 60)
+        days, hours = divmod(hours, 24)
+
+        duration = []
+        if days > 0:
+            duration.append(f"{days} days")
+        if hours > 0:
+            duration.append(f"{hours} hours")
+        if minutes > 0:
+            duration.append(f"{minutes} minutes")
+        if seconds > 0:
+            duration.append(f"{seconds} seconds")
+
+        return ', '.join(duration)
