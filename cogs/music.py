d3248f378577df98127679c35082655f8549dba5
parzuko <jivansh17@gmail.com>
2020-09-17 22:09:58 +0530

    setup basic ytdl and ffmpeg checks

--- cogs/music.py
+++ cogs/music.py
@@ -10,3 +10,26 @@ class VoiceError(Exception):
 
 class YTDLError(Exception):
     pass
+
+class YTDLSource(discord.PCMVolumeTransformer):
+    YTDL_OPTIONS = {
+        "format" : "bestaudio/best",
+        "audioformat" : "mp3",
+        "outtmpl": "%(extractor)s-%(id)s-%(title)s.%(ext)s",
+        "restrictfilenames" : True,
+        "extractaudio" : True,
+        "noplaylist" : True,
+        "nocheckcertificate" : True,
+        "ignoreerrors" : False,
+        "logtostderr"": False,
+        "quiet": True,
+        "no_warnings": True,
+        "default_search": "auto",
+        "source_address": "0.0.0.0",
+    }
+    FFMPEG_OPTIONS = {
+        "before_options": "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5",
+        "options": "-vn",
+    }
+
+    ytdl = youtube_dl.YoutubeDL(YTDL_OPTIONS)
