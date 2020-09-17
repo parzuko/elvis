1196175561c2ce8280d5c21805726fc2926a66a4
parzuko <jivansh17@gmail.com>
2020-09-17 11:32:55 +0530

    add clean to basic

--- cogs/basic.py
+++ cogs/basic.py
@@ -7,17 +7,20 @@ class Basic(commands.Cog):
     def __init__(self, elvis):
         self.elvis = elvis
 
-    # Events
     @commands.Cog.listener()
     async def on_ready(self):
         print("Elvis is ready!")
-
-    # Commmands
+    
     @commands.command(aliases = ["Elvis", "sun", "hello"], name = "elvis")
     async def _introduce(self, ctx):
         """Elvis says hi."""
 
         await ctx.send("Hi! I'm Elvis. Here to listen to all your needs😁. Just type ' .help ' to learn about what I can do! ")
+
+    @commands.command(name = "clear", aliases = ["saaf", "clean", "Clear", "c"])
+    async def _clear(self, ctx, amount = 5):
+        await ctx.channel.purge(limit = amount)
+        await ctx.send(f"Deleted previous {amount} messages! 🧹🧼🧽 ")
 
 def setup(elvis):
     elvis.add_cog(Basic(elvis))
