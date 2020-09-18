757425ce05a84d00af1291de02604c9fba5ba2b7
parzuko <jivansh17@gmail.com>
2020-09-18 11:15:58 +0530

    finish the voicestate class with all parent functions

--- cogs/music.py
+++ cogs/music.py
@@ -21,30 +21,66 @@ class VoiceState:
         self._volume = 0.5
         self.skip_votes = set()
         self.audio_player = elvis.loop.create_task(self.audio_player_task())
-        
 
+    def __del__(self):
+        self.audio_player.cancel()
 
+    @property
+    def loop(self):
+        return self._loop
+    
+    @loop.setter
+    def loop(self, value: bool):
+        self._loop = value
 
+    @property
+    def volume(self):
+        return self._volume
 
+    @volume.setter
+    def volume(self, value: float):
+        self._volume = value
 
+    @property
+    def is_playing(self):
+        return self.voice and self.current
 
+    async def audio_player_task(self):
+        while True:
+            self.next.clear()
 
+            if not self.loop:
+                try:
+                    async with timeout(240):  # 4 minutes
+                        self.current = await self.songs.get()
+                except asyncio.TimeoutError:
+                    self.elvis.loop.create_task(self.stop())
+                    return
 
+            self.current.source.volume = self._volume
+            self.voice.play(self.current.source, after=self.play_next_song)
+            await self.current.source.channel.send(embed=self.current.create_embed())
 
+            await self.next.wait()
 
+    def play_next_song(self, error=None):
+        if error:
+            raise VoiceError(str(error))
 
+        self.next.set()
 
+    def skip(self):
+        self.skip_votes.clear()
 
+        if self.is_playing:
+            self.voice.stop()
 
+    async def stop(self):
+        self.songs.clear()
 
-
-
-
-
-
-
-
-
+        if self.voice:
+            await self.voice.disconnect()
+            self.voice = None
 
 # Incase of YTDL OR FFFMPEGG Erros
 youtube_dl.utils.bug_reports_message = lambda: ''
