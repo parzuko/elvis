ac446ef498b819cef2a62b5f0b7a482e4e62b50e
parzuko <jivansh17@gmail.com>
2020-09-18 11:08:40 +0530

    finish all helper classes for music cog

--- cogs/music.py
+++ cogs/music.py
@@ -147,3 +147,25 @@ class Song:
 
         return embed
     
+class SongQueue(asyncio.Queue):
+    def __getitem__(self, item):
+        if isinstance(item, slice):
+            return list(itertools.islice(self._queue, item.start, item.stop, item.step))
+        else:
+            return self._queue[item]
+
+    def __iter__(self):
+        return self._queue
+    
+    def __len__(self):
+        return self.qsize()
+
+    def clear(self):
+        self.queue.clear()
+    
+    def shuffle(self):
+        random.shuffle(self,queue)
+    
+    def remove(self, index: int):
+        del self._queue[index]
+    
