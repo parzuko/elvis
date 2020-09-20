dc347064d95bb7110308da73e19f281450249ca6
parzuko <jivansh17@gmail.com>
2020-09-20 13:31:45 +0530

    finish leveling lgoci

--- cogs/xp.py
+++ cogs/xp.py
@@ -28,17 +28,30 @@ class XP_Leveling(commands.Cog):
             xp = self.get_xp()
             print(f"{message.author.name} gets {xp}xp")
             cursor = mydb.cursor()
-            cursor.execute(f"SELECT user_xp FROM users WHERE client_id = {message.author.id}")
+            cursor.execute(f"SELECT user_xp, user_level FROM users WHERE client_id = {message.author.id}")
             result = cursor.fetchall()
             if len(result) == 0:
                 print("addig to db")
-                cursor.execute(f"INSERT INTO users VALUES({message.author.id},{xp})")
+                cursor.execute(f"INSERT INTO users VALUES({message.author.id},{xp}, 1)")
                 mydb.commit()
                 print("done")
             else:
                 new_xp = result[0][0] + xp
+                current_level = result[0][1]
+
+
+                if new_xp < 20:
+                    current_level = 1
+                elif new_xp > 20 and new_xp < 40:
+                    current_level = 2
+                elif new_xp > 40 and new_xp < 60:
+                    current_level = 3
+
+
+
                 print(f"new = {new_xp}") 
-                cursor.execute(f"UPDATE users SET user_xp = {new_xp} WHERE client_id = {message.author.id}")
+                print(f"new level = {current_level}")
+                cursor.execute(f"UPDATE users SET user_xp = {new_xp}, user_level = {current_level} WHERE client_id = {message.author.id}")
                 mydb.commit()
 
 def setup(elvis):
